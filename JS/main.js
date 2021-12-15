f4d75cfa19b6b1d41709c3ad52664ab640cc0e3a
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-12-16 00:19:34 +0530

    about and brands completed

--- JS/main.js
+++ JS/main.js
@@ -19,10 +19,11 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000);
+    setTimeout(showSlides, 8000);
 }
 
 function moveSlides(position) {
+    console.log("Move")
     slideIndex += position;
     if (slideIndex > slides.length) { slideIndex = 1 }
     else if (slideIndex < 1) { slideIndex = slides.length }
@@ -51,21 +52,58 @@ function currentSlide(index) {
 
 
 
-// SLider for Best Seller
-var slideIndex = 3;
-showDivs(slideIndex);
+// Slider for Best Seller
+var bestIndex = 1;
+showDivs(bestIndex);
 
 function plusDivs(n) {
-    showDivs(slideIndex += n);
+    showDivs(bestIndex += n);
 }
 
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("slide__watches");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length };
+    if (n > x.length) { bestIndex = 1 }
+    if (n < 1) { bestIndex = x.length };
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-    x[slideIndex - 1].style.display = "flex";
+    x[bestIndex - 1].style.display = "flex";
+}
+
+// Slider for Brands
+var slideBIndex = 0;
+showBSlides();
+var bSlides, bDots;
+
+function showBSlides() {
+    var i;
+    // get slides length
+    bSlides = document.getElementsByClassName("brand__slide");
+    // get dots length
+    bDots = document.getElementsByClassName("bdot");
+    for (i = 0; i < bSlides.length; i++) {
+        bSlides[i].style.display = "none";
+    }
+    slideBIndex++;
+    if (slideBIndex > bSlides.length) { slideBIndex = 1 }
+    for (i = 0; i < bDots.length; i++) {
+        bDots[i].className = bDots[i].className.replace(" active", "");
+    }
+    bSlides[slideBIndex - 1].style.display = "flex";
+    bDots[slideBIndex - 1].className += " active";
+    setTimeout(showBSlides, 2000);
+}
+
+function currentBSlide(index) {
+    if (index > bSlides.length) { index = 1 }
+    else if (index < 1) { index = bSlides.length }
+    for (i = 0; i < bSlides.length; i++) {
+        bSlides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        bDots[i].className = bDots[i].className.replace(" active", "");
+    }
+    bSlides[index - 1].style.display = "flex";
+    bDots[index - 1].className += " active";
 }
